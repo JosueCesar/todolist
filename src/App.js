0162c0ecf19421ec641761c9ba3dca05f31cf5b8
Josue Cesar <mendonca.josue@gmail.com>
2020-03-14 13:07:20 -0300

    pronto para iniciar o desenvolvimento

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Routes from './routes';
+import GlobalStyle from './styles/Global';
+
+const app = () => {
+  return(
+    <>
+      <Routes />
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default app;
---
+++ src/styles/Global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    border: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: 0;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  body {
+    background-color: #24252a;
+  }
+`;
